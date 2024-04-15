f3d3c22376017b15e6b197c427a0439f54f43672
illakaya <mimijane.nguyen@gmail.com>
2024-04-15 17:12:59 +1000

    changed the api call from 2 (geocoding, weather), so a single api call using query string

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,11 @@
 const apiKey = "bb2b7f5666002551c33de32d02707e73";
 
-function getCoordinates (city) {
-    const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
-
-    fetch(apiUrl)
-        .then(function (response) {
-            if(response.ok) {
-                response.json().then(function (data) {
-                    getWeather(data[0].lat, data[0].lon);
-                })
-            }
-        })
-    
-}
-
-function getWeather (lat, lon) {
-    const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+function getWeather (city) {
+    // lets users enter cities with spaces, removes the spaces in the city
+    function removeSpaces(str) {
+        return str.split(' ').join('');
+    }
+    const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${removeSpaces(city)}&units=metric&appid=${apiKey}`;
 
     fetch(apiUrl)
         .then(function (response) {
@@ -23,6 +13,8 @@ function getWeather (lat, lon) {
                 response.json().then(function (data) {
                     console.log(data);
                 })
+            } else {
+                alert(`Error: ${city} ${response.statusText}`);
             }
-        })
+        }) 
 }
